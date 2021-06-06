38cd45989b5833636682641b54ff821f71f3678f
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-06-05 20:12:32 -0700

    tic-tac-toe-node class complete passing all tests

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,6 +1,5 @@
 require_relative 'tic_tac_toe'
 
-
 class TicTacToeNode
   attr_accessor :board, :next_mover_mark, :prev_move_pos
 
@@ -23,9 +22,27 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    if board.over?
+      return board.won? && board.winner != evaluator
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.all? { |node| node.losing_node?(evaluator) }
+    else
+      self.children.any? { |node| node.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
+    if board.over?
+      return board.winner == evaluator
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.all? { |node| node.winning_node?(evaluator) }
+    else
+      self.children.any? { |node| node.winning_node?(evaluator) }
+    end
   end
 
   # This method generates an array of all moves that can be made after
@@ -46,8 +63,6 @@ class TicTacToeNode
 
     children_nodes
   end
-
-
 end
 
 
